e8688b34705a6764058ecd4ca86b6d3aad319a2c
mwaiyusuf <mwaigalo5@gmail.com>
2019-03-02 14:59:17 +0300

    javasccript

--- js/custom.js
+++ js/custom.js
@@ -1,34 +1,34 @@
 var total=0
 var values=[];
-
+ 
 function roll() {
-    alert("hi");
+   
  values.push(document.getElementById('pl1').innerHTML= parseInt(Math.random()*6+1));
  alert(values)
 }
 
-//$(document).ready(function(){
-// $("#rol").click(function(){
-//     
-//   var nax=0;
-//   for(z=0;z<values.length;z++){
-//     if(values[z]!=1){
-//        nax=nax+values[z];
-//     }
-//     else {
-//       values.length=0;
-//     }
-//
-//   }
-//   alert(nax)
-// })
-//
-//})
-//
-//
-////calculate total score here
-//
-//
-//function roll2() {
-// document.getElementById('pl2').innerHTML= parseInt(Math.random()*6+1);
-//}
+$(document).ready(function(){
+ $("#hold").click(function(){
+     alert("hi");
+   var nax=0;
+   for(z=0;z<values.length;z++){
+     if(values[z]!=1){
+        nax=nax+values[z];
+     }
+     else {
+       values.length=0;
+     }
+
+   }
+   alert(nax)
+ })
+
+})
+
+
+//calculate total score here
+
+
+function roll2() {
+ document.getElementById('pl2').innerHTML= parseInt(Math.random()*6+1);
+}

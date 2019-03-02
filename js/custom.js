3dcc82c9959ef527f67951c522417dc14f957755
mwaiyusuf <mwaigalo5@gmail.com>
2019-03-02 22:12:42 +0300

    current score

--- js/custom.js
+++ js/custom.js
@@ -1,36 +1,52 @@
-var totalValues[];
-var values=[];
-var summation=[];
- 
+//var totalValues[];
+//var values=[];
+//var summation=[];
+// 
+//function roll() {
+//   
+// values.push(document.getElementById('pl1').innerHTML= parseInt(Math.random()*6+1));
+// alert(values)
+//}
+//
+//$(document).ready(function(){
+// $("#hold").click(function(){
+//     alert("hi");
+//   var nax=0;
+//   for(i=0;i<values.length;i++){
+//     if(values[i]!==1){
+//        nax=nax+values[i];
+//     }
+//     else {
+////       values.length=0;
+//         values=0;
+//     }
+//
+//   }
+//   alert(nax);
+// })
+//
+//})
+//
+//
+////calculate total score here
+//
+//
+//function roll2() {
+// document.getElementById('pl2').innerHTML= parseInt(Math.random()*6+1);
+//}
+var totalSum=[];
+var sum=[];
+var currentScore=0;
 function roll() {
-   
- values.push(document.getElementById('pl1').innerHTML= parseInt(Math.random()*6+1));
- alert(values)
-}
-
-$(document).ready(function(){
- $("#hold").click(function(){
-     alert("hi");
-   var nax=0;
-   for(i=0;i<values.length;i++){
-     if(values[i]!==1){
-        nax=nax+values[i];
-     }
-     else {
-//       values.length=0;
-         values=0;
-     }
-
-   }
-   alert(nax);
- })
-
-})
-
-
-//calculate total score here
-
-
-function roll2() {
- document.getElementById('pl2').innerHTML= parseInt(Math.random()*6+1);
-}
+    var rollDice=document.getElementById('pl1').innerHTML=parseInt(Math.random()*6+1);
+     
+    if(rollDice!=1){
+        currentScore+=rollDice;
+        alert("the current score is" + currentScore);
+        
+    }
+    else{
+     currentScore=0;
+    }
+    
+};
